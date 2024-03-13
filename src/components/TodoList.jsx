a3430dabc48fff4b54f134594a411ddb2b764a0b
Rahulchiluka0 <96143034+Rahulchiluka0@users.noreply.github.com>
2024-03-13 11:42:10 +0530

    Update TodoList.jsx

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,12 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './TodoList.css'; // Import CSS file
 import { verse } from "../assets/verses.json";
+
+
+
 let randomId = Math.floor(Math.random() * 701) + 1;
 let shlok = verse[randomId].text;
 
 function TodoApp() {
-    const [todos, setTodos] = useState([]);
+    const [todos, setTodos] = useState(() => JSON.parse(localStorage.getItem("todos")) || []);
     const [inputValue, setInputValue] = useState('');
+
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -25,39 +33,39 @@ function TodoApp() {
         setTodos(todos.filter(todo => todo.id !== id));
     };
     return (
-    <div className='container'>
-        <div className="todo-container">
-            <h1>The-Bhagwad-Geeta</h1>
-            <form onSubmit={handleSubmit} className="todo-form">
-                <div className="input-with-button">
-                    <input
-                        type="text"
-                        placeholder="Enter your todo..."
-                        value={inputValue}
-                        onChange={(e) => setInputValue(e.target.value)}
-                        required
-                    />
-                    <button type="submit">Add Todo</button>
-                </div>
-            </form>
-            <h2>Your Tasks</h2>
-            {todos.length === 0 ? (
-                <div className='shloka'><p>{shlok}</p></div>
-            ) : (
-                <ul className="todo-list">
-                    {todos.map(todo => (
-                        <li key={todo.id} className={todo.completed ? 'completed' : ''}>
-                            <span
-                                onClick={() => handleComplete(todo.id)}
-                            >
-                                {todo.text}
-                            </span>
-                            <button onClick={() => handleDelete(todo.id)}>Delete</button>
-                        </li>
-                    ))}
-                </ul>)}
+        <div className='container'>
+            <div className="todo-container">
+                <h1>The-Bhagwad-Geeta</h1>
+                <form onSubmit={handleSubmit} className="todo-form">
+                    <div className="input-with-button">
+                        <input
+                            type="text"
+                            placeholder="Enter your todo..."
+                            value={inputValue}
+                            onChange={(e) => setInputValue(e.target.value)}
+                            required
+                        />
+                        <button type="submit">Add Todo</button>
+                    </div>
+                </form>
+                <h2>Your Tasks</h2>
+                {todos.length === 0 ? (
+                    <div className='shloka'><p>{shlok}</p></div>
+                ) : (
+                    <ul className="todo-list">
+                        {todos.map(todo => (
+                            <li key={todo.id} className={todo.completed ? 'completed' : ''}>
+                                <span
+                                    onClick={() => handleComplete(todo.id)}
+                                >
+                                    {todo.text}
+                                </span>
+                                <button onClick={() => handleDelete(todo.id)}>Delete</button>
+                            </li>
+                        ))}
+                    </ul>)}
+            </div>
         </div>
-    </div>
 
     );
 }
